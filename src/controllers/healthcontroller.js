41e5bb79e4572b65a95b5eba732695a6f4ea5ab0
Abhinav27srivastava <srivastavaabhinav307@gmail.com>
2026-07-24 16:34:52 +0530

    Add database health check endpoint

--- src/controllers/healthcontroller.js
+++ src/controllers/healthcontroller.js
@@ -1,7 +1,25 @@
-const healthCheck = (req,res) =>{
+const pool = require('../config/db');
+
+
+
+const healthCheck = async (req,res) =>{
+
+  try {
+    await pool.query("select 1")
     res.status(200).json({
-        status: "healthy"
+        status: "healthy",
+        databse: "connected"
     });
-};
+    }
+
+  catch(err)
+  {
+   res.status(503).json({   // 503 means server is running but some dependecy is not availaible
+    status: "unhealthy",
+    database: "disconnected"
+   })
+  }  
+  
+} ;
 
 module.exports = {healthCheck};
