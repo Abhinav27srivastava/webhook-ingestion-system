4b6ef13660c0856ae1c0a13c92a20f06a81e9831
Abhinav27srivastava <srivastavaabhinav307@gmail.com>
2026-08-04 21:34:05 +0530

    Fix authentication middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,11 +4,12 @@ const authenticate = (req,res,next)=>{
     //get authorization Header from request
     const authHeader = req.headers.authorization;
     // checking if authorization header is present and starts with Bearer then return 401 unauthorized if not present   if(!authHeader || !authHeader.startsWith("Bearer ")){ 
+   if(!authHeader || !authHeader.startsWith("Bearer ")){
     return res.status(401).json({
         success: false,
         message: "Unauthorized access. Token missing"
     });
-    }
+}
  //extract token from authorization header
  const token = authHeader.split(" ")[1]; 
  try{
@@ -23,6 +24,8 @@ catch(error ){
         success: false,
         message: "Unauthorized access. Invalid token"
     });
+}
  };
+
  module.exports = authenticate;
 
