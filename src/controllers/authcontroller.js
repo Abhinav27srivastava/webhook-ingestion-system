a06d8e5bad13fa3c25a918541a12de5673ec4150
Abhinav27srivastava <srivastavaabhinav307@gmail.com>
2026-08-05 19:58:31 +0530

    admin window

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -95,7 +95,8 @@ const login = async (req, res) => {
         const token = jwt.sign(
             {
                 id: user.id,
-                email: user.email
+                email: user.email,
+                role:user.role
             },
             process.env.JWT_SECRET,
             {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,8 +5,16 @@ const { register } = require("../controllers/authcontroller");
 const {login} = require("../controllers/authcontroller");
 const { profile } = require("../controllers/authcontroller");
 const authenticate = require("../middleware/authMiddleware");
+const {authorize} = require("../middleware/authorize");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get('/profile', authenticate, profile);
+router.get('/admin',authenticate,authorize('admin'),(req,res)=>{
+    return res.status(200).json({
+        success: true,
+        message: "welcome  admin",
+       
+    });
+});
 module.exports = router;
